1e212d91813c6b1907295889f793d6949f1e702e
Charlie Cheever <ccheever@gmail.com>
2015-06-11 00:05:31 -0700

    Moving to an EventEmitter based model
    Fixing some bugs and handling some errors better

--- build/index.js
+++ build/index.js
@@ -4,33 +4,53 @@ var _asyncToGenerator = require('babel-runtime/helpers/async-to-generator')['def
 
 var _Promise = require('babel-runtime/core-js/promise')['default'];
 
-var syncLoop = _asyncToGenerator(function* (client, cursor, destFolder, opts) {
+var syncLoop = _asyncToGenerator(function* (client, cursor, destFolder, emitter, opts, flags) {
 
   opts = opts || {};
   yield mkdirp.promise(destFolder);
-  var newCursor = yield getAsync(client, cursor, destFolder, opts);
+  if (flags.stop) {
+    emitter.emit('stopped', cursor);
+    return;
+  }
+  var newCursor = yield getAsync(client, cursor, destFolder, emitter, opts);
+  if (flags.stop) {
+    emitter.emit('stopped', cursor);
+    return;
+  }
   var result;
   do {
     if (opts.verbose) {
       console.log('Polling for changes...');
     }
     result = yield client.promise.pollForChanges(newCursor);
+    if (opts.verbose) {
+      console.log('Polled successfuly');
+    }
+    if (flags.stop) {
+      emitter.emit('stopped', cursor);
+      return;
+    }
     if (!result) {
-      if (opts.verbose) {
-        console.error('Failure! No response from `pollForChanges`');
-      }
+      console.error('Failure! No response from `pollForChanges`');
       // TODO: Implement a smart back-off strategy
       result = { hasChanges: false, retryAfter: 30000 };
     }
     if (!result.hasChanges && result.retryAfter) {
       yield delayAsync(result.retryAfter * 1000);
+      if (flags.stop) {
+        return;
+      }
     }
-  } while (!result.hasChanges);
+  } while (!result.hasChanges && !flags.stop);
 
-  return syncLoop(client, newCursor, destFolder, opts);
+  if (flags.stop) {
+    emitter.emit('stopped', cursor);
+    return;
+  }
+  return syncLoop(client, newCursor, destFolder, emitter, opts, flags);
 });
 
-var getAsync = _asyncToGenerator(function* (client, cursor, destFolder, opts) {
+var getAsync = _asyncToGenerator(function* (client, cursor, destFolder, emitter, opts) {
 
   var delta = yield client.promise.delta(cursor);
   if (!delta) {
@@ -39,27 +59,42 @@ var getAsync = _asyncToGenerator(function* (client, cursor, destFolder, opts) {
   var newCursor = delta.cursorTag;
   var awaitables = [];
   for (var change of delta.changes) {
-    awaitables.push(processChangeAsync(client, change, destFolder, opts));
+    emitter.emit('changeTo', change.path, change);
+    awaitables.push(processChangeAsync(client, change, destFolder, emitter, opts));
   }
-  yield _Promise.all(awaitables);
+  try {
+    yield _Promise.all(awaitables);
+  } catch (e) {
+    console.error('Error syncing (but will continue anyway):', e.message);
+    emitter.emit('errorIgnored', e);
+    // Wait 15 seconds
+    yield delayAsync(15000);
+    // Return the old cursor since the sync failed
+    return cursor;
+  }
 
+  //console.log("Synced as of", Date.now());
+  emitter.emit('syncedToCursor', newCursor);
   return newCursor;
 });
 
-var processChangeAsync = _asyncToGenerator(function* (client, change, destFolder, opts) {
+var processChangeAsync = _asyncToGenerator(function* (client, change, destFolder, emitter, opts) {
   opts = opts || {};
   var dest = path.join(destFolder, change.path);
   if (opts.verbose) {
     console.log('Syncing', dest, '...');
   }
   if (change.wasRemoved) {
-    yield fs.promise.unlink(change.path);
+    yield fs.promise.unlink(dest);
+    emitter.emit('didRemove', dest, change);
   } else if (change.stat.isFolder) {
     yield mkdirp.promise(dest);
+    emitter.emit('didMkdirp', dest, change);
   } else if (change.stat.isFile) {
     // nesh*> yield fs.promise.writeFile('/tmp/image.jpg', yield c.promise.readFile('/dellie.jpg', {buffer:true}))
     var contentsBuffer = yield client.promise.readFile(change.path, { buffer: true });
     yield fs.promise.writeFile(dest, contentsBuffer);
+    emitter.emit('didWriteFile', dest, change);
   } else {
     throw new Error('I\'m confused and don\'t know how to deal with this change', change);
   }
@@ -70,6 +105,7 @@ var delayAsync = require('delay-async');
 var dropbox = require('dropbox');
 var events = require('events');
 var fs = require('fs');
+var moment = require('moment-timezone');
 var mkdirp = require('mkdirp');
 var path = require('path');
 var secret = require('@exponent/secret');
@@ -82,11 +118,35 @@ function getClient() {
   });
 }
 
+function startSyncing(client, destFolder, opts) {
+  opts = opts || {};
+  var cursor = opts.cursor || null;
+  delete opts.cursor;
+  if (opts.verbose) {
+    console.log('Created destination folder', destFolder);
+  }
+
+  var emitter = new events.EventEmitter();
+  emitter._flags = {};
+  emitter.stopSyncingAsync = function () {
+    emitter._flags.stop = true;
+    return new _Promise(function (fulfill, reject) {
+      emitter.on('stopped', fulfill);
+    });
+  };
+
+  syncLoop(client, cursor, destFolder, emitter, opts, emitter._flags).then(console.log, console.error);
+
+  return emitter;
+}
+
 module.exports = {
   getAsync: getAsync,
   getClient: getClient,
   processChangeAsync: processChangeAsync,
   secret: secret,
-  syncLoop: syncLoop
+  syncLoop: syncLoop,
+  startSyncing: startSyncing,
+  c: getClient()
 };
 //# sourceMappingURL=sourcemaps/index.js.map
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ var delayAsync = require('delay-async');
 var dropbox = require('dropbox');
 var events = require('events');
 var fs = require('fs');
+var moment = require('moment-timezone');
 var mkdirp = require('mkdirp');
 var path = require('path');
 var secret = require('@exponent/secret');
@@ -15,35 +16,79 @@ function getClient() {
   });
 }
 
-async function syncLoop(client, cursor, destFolder, opts) {
+function startSyncing(client, destFolder, opts) {
+  opts = opts || {};
+  var cursor = opts.cursor || null;
+  delete opts.cursor;
+  if (opts.verbose) {
+    console.log("Created destination folder", destFolder);
+  }
+
+  var emitter = new events.EventEmitter();
+  emitter._flags = {};
+  emitter.stopSyncingAsync = function () {
+    emitter._flags.stop = true;
+    return new Promise((fulfill, reject) => {
+      emitter.on('stopped', fulfill);
+    });
+  }
+
+  syncLoop(client, cursor, destFolder, emitter, opts, emitter._flags).then(console.log, console.error);
+
+  return emitter;
+
+}
+
+async function syncLoop(client, cursor, destFolder, emitter, opts, flags) {
 
   opts = opts || {};
   await mkdirp.promise(destFolder);
-  var newCursor = await getAsync(client, cursor, destFolder, opts);
+  if (flags.stop) {
+    emitter.emit('stopped', cursor);
+    return;
+  }
+  var newCursor = await getAsync(client, cursor, destFolder, emitter, opts);
+  if (flags.stop) {
+    emitter.emit('stopped', cursor);
+    return;
+  }
   var result;
   do {
     if (opts.verbose) {
       console.log("Polling for changes...");
     }
     result = await client.promise.pollForChanges(newCursor);
+    if (opts.verbose) {
+      console.log("Polled successfuly");
+    }
+    if (flags.stop) {
+      emitter.emit('stopped', cursor);
+      return;
+    }
     if (!result) {
-      if (opts.verbose) {
-        console.error("Failure! No response from `pollForChanges`");
-      }
+      console.error("Failure! No response from `pollForChanges`");
       // TODO: Implement a smart back-off strategy
       result = {hasChanges: false, retryAfter: 30000};
     }
     if (!result.hasChanges && result.retryAfter) {
       await delayAsync(result.retryAfter * 1000);
+      if (flags.stop) {
+        return;
+      }
     }
-  } while (!result.hasChanges);
+  } while (!result.hasChanges && !flags.stop);
 
-  return syncLoop(client, newCursor, destFolder, opts);
+
+  if (flags.stop) {
+    emitter.emit('stopped', cursor);
+    return;
+  }
+  return syncLoop(client, newCursor, destFolder, emitter, opts, flags);
 
 }
 
 
-async function getAsync(client, cursor, destFolder, opts) {
+async function getAsync(client, cursor, destFolder, emitter, opts) {
 
   var delta = await client.promise.delta(cursor);
   if (!delta) {
@@ -52,28 +97,43 @@ async function getAsync(client, cursor, destFolder, opts) {
   var newCursor = delta.cursorTag;
   var awaitables = [];
   for (var change of delta.changes) {
-    awaitables.push(processChangeAsync(client, change, destFolder, opts));
+    emitter.emit('changeTo', change.path, change);
+    awaitables.push(processChangeAsync(client, change, destFolder, emitter, opts));
   }
-  await Promise.all(awaitables);
+  try {
+    await Promise.all(awaitables);
+  } catch (e) {
+    console.error("Error syncing (but will continue anyway):", e.message);
+    emitter.emit('errorIgnored', e);
+    // Wait 15 seconds
+    await delayAsync(15000);
+    // Return the old cursor since the sync failed
+    return cursor;
+  }
 
+  //console.log("Synced as of", Date.now());
+  emitter.emit('syncedToCursor', newCursor);
   return newCursor;
 
 }
 
-async function processChangeAsync(client, change, destFolder, opts) {
+async function processChangeAsync(client, change, destFolder, emitter, opts) {
   opts = opts || {};
   var dest = path.join(destFolder, change.path);
   if (opts.verbose) {
     console.log("Syncing", dest, "...");
   }
   if (change.wasRemoved) {
-    await fs.promise.unlink(change.path);
+    await fs.promise.unlink(dest);
+    emitter.emit('didRemove', dest, change);
   } else if (change.stat.isFolder) {
     await mkdirp.promise(dest);
+    emitter.emit('didMkdirp', dest, change);
   } else if (change.stat.isFile) {
     // nesh*> yield fs.promise.writeFile('/tmp/image.jpg', yield c.promise.readFile('/dellie.jpg', {buffer:true}))
     var contentsBuffer = await client.promise.readFile(change.path, {buffer: true});
     await fs.promise.writeFile(dest, contentsBuffer);
+    emitter.emit('didWriteFile', dest, change);
   } else {
     throw new Error("I'm confused and don't know how to deal with this change", change);
   }
@@ -87,4 +147,6 @@ module.exports = {
   processChangeAsync,
   secret,
   syncLoop,
+  startSyncing,
+  c: getClient(),
 };
